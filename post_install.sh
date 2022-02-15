30e6ff54c51ff664ee4361a51566af40dc47e720
jasker5183 <4397623+jasker5183@users.noreply.github.com>
2022-02-15 02:57:27 -0600

    Add files via upload
    Make file executable

---
+++ post_install.sh
@@ -0,0 +1,9 @@
+sysrc -f /etc/rc.conf prowlarr_enable="YES"
+
+#62991
+chown -R prowlarr:prowlarr /usr/local/share/prowlarr
+
+#we need write permission to be able to write plugins update. #53127
+chmod 755 /usr/local/prowlarr
+
+service prowlarr start
